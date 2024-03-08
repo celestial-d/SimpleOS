ebca6f94bdf1687d7788273f84672ddb26e21245
celestial <duozhang@iastate.edu>
2024-03-08 11:15:47 -0800

    syscall up for fs

--- oskernel/kernel/system_call.c
+++ oskernel/kernel/system_call.c
@@ -3,13 +3,16 @@
 #include "../include/linux/sys.h"
 #include "../include/linux/task.h"
 #include "../include/linux/sched.h"
+#include "../include/d_shell.h"
+#include "../include/string.h"
 
 #define SYSTEM_CALL_TABLE_SIZE  64
 
 extern task_t* current;
 
 void* system_call_table[SYSTEM_CALL_TABLE_SIZE] = {
-        sys_write, sys_exit, sys_fork, sys_get_pid, sys_get_ppid
+        sys_write, sys_exit, sys_fork, sys_get_pid, sys_get_ppid, sys_active_ya_shell,
+        sys_fopen, sys_fclose, sys_fread, sys_fwrite
 };
 
 ssize_t sys_write(int fd, const void *buf, size_t count) {
@@ -29,4 +32,42 @@ pid_t sys_get_pid() {
 
 pid_t sys_get_ppid() {
     return get_task_ppid(current);
+}
+
+void sys_active_ya_shell() {
+    active_d_shell();
+}
+
+file_t* sys_fopen(const char *filename, const char *mode) {
+    int flag = O_RDONLY;
+
+    // mode to flag
+
+    return kfopen(filename, flag);
+}
+
+int sys_fclose(FILE *stream) {
+    if (NULL == stream) {
+        return -1;
+    }
+
+    return kfclose(stream);
+}
+
+size_t sys_fread(void *ptr, size_t size, FILE *stream) {
+    if (NULL == ptr || 0 == size || NULL == stream) {
+        return -1;
+    }
+
+    memset(ptr, 0, size);
+
+    return kfread(ptr, size, stream);
+}
+
+size_t sys_fwrite(const void *ptr, size_t size, FILE *stream) {
+    if (NULL == ptr || 0 == size || NULL == stream) {
+        return -1;
+    }
+
+    return kfwrite(ptr, size, stream);
 }
