ecca05729382ad03d579ed406f19c951b8c70488
celestial <duozhang@iastate.edu>
2024-03-08 11:35:56 -0800

    unistd up for fs

--- oskernel/lib/unistd.c
+++ oskernel/lib/unistd.c
@@ -16,3 +16,39 @@ pid_t getppid() {
 
     return pid;
 }
+
+void uactive_d_shell() {
+    __asm__ __volatile__ ("int 0x80"::"a"(__NR_active_d_shell));
+}
+
+FILE* fopen(const char *filename, const char *mode) {
+    FILE* file = NULL;
+
+    __asm__ __volatile__ ("int 0x80":"=a"(file):"0"(__NR_fopen), "b"(filename), "c"(mode));
+
+    return file;
+}
+
+int fclose(FILE *stream) {
+    int ret = -1;
+
+    __asm__ __volatile__ ("int 0x80":"=a"(ret):"0"(__NR_fclose), "b"(stream));
+
+    return ret;
+}
+
+size_t fread(void *ptr, size_t size, FILE *stream) {
+    size_t ret = -1;
+
+    __asm__ __volatile__ ("int 0x80":"=a"(ret):"0"(__NR_fread), "b"(ptr), "c"(size), "d"(stream));
+
+    return ret;
+}
+
+size_t fwrite(const void *ptr, size_t size, FILE *stream) {
+    size_t ret = -1;
+
+    __asm__ __volatile__ ("int 0x80":"=a"(ret):"0"(__NR_fwrite), "b"(ptr), "c"(size), "d"(stream));
+
+    return ret;
+}
